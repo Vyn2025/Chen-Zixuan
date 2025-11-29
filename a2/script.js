0a52654d74e62535cab1241719d11c627b9b62a8
Vyn2025 <zixuanc360@gmail.com>
2025-11-29 16:22:39 +0800

    Delete a2 directory

--- a2/script.js
+++
@@ -1,9 +0,0 @@
-function hamburger() {
-    const menu = document.getElementById("menu-links");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    }
-    else {
-        menu.style.display = "block";
-    }
-}
